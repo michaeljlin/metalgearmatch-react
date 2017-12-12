bf3a35369f812307a774ae3f1f1ef69f75789e4b
michaeljlin <mjameslin@gmail.com>
2017-12-12 14:06:45 -0800

    Set up main components & basic click functions

--- src/components/card.js
+++ src/components/card.js
@@ -1,23 +1,85 @@
 import React, { Component } from 'react';
+import backImg from '../assets/images/mgscard.svg';
+import './app.css';
 
 class Card extends Component{
     constructor(props) {
         super(props);
 
         this.state = {
-            num: this.props.num
+            num: this.props.num,
+            frontStyle: {
+                'backgroundColor': 'blue',
+                display: 'none'
+            },
+            backStyle: {
+                'backgroundColor': 'red',
+                display: 'block'
+            }
         };
 
         this.handleClick = this.handleClick.bind(this);
     }
 
+    componentWillReceiveProps(nextProps){
+        console.log('got message');
+        if(nextProps.display){
+            this.setState({
+                frontStyle: {
+                    'backgroundColor': 'blue',
+                    display: 'none'
+                },
+                backStyle: {
+                    'backgroundColor': 'red',
+                    display: 'block'
+                }
+            });
+        }
+        // console.log(this.state);
+    }
+
     handleClick(){
         console.log(`you got ${this.state.num}!`);
+
+        if(this.state.frontStyle.display === 'none'){
+            this.setState({
+                frontStyle:{
+                    'backgroundColor': 'blue',
+                    display: 'block'
+                },
+                backStyle: {
+                    'backgroundColor': 'red',
+                    display: 'none'
+                }
+            });
+        }
+        else{
+            this.setState({
+                frontStyle:{
+                    'backgroundColor': 'blue',
+                    display: 'none'
+                },
+                backStyle: {
+                    'backgroundColor': 'red',
+                    display: 'block'
+                }
+            });
+        }
     }
 
     render(){
+        const {frontStyle, backStyle} = this.state;
+
         return(
-            <div onClick={this.handleClick}>{this.props.num}</div>
+            <div>
+                <div className={"card"} style={frontStyle} onClick={this.handleClick}>
+                    <img src={backImg}/>
+                </div>
+                <div className={"card"} style={backStyle} onClick={this.handleClick}>
+                    <img src={backImg}/>
+                </div>
+                {this.props.num}
+            </div>
         );
     }
 }
--- src/components/mainboard.js
+++ src/components/mainboard.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import Player from "./player";
 import Boss from "./boss";
 import Card from './card';
+import Deck from './deck';
+
+import backImg from '../assets/images/mgscard.svg';
 
 class Main extends Component{
     constructor(props){
         super(props);
 
         this.state = {
-            cards: [1,2,3]
+            cards: [1,2,3],
+            reset: false
         };
 
         this.dealcards = this.dealcards.bind(this);
         this.shufflecards = this.shufflecards.bind(this);
+        this.reset = this.reset.bind(this);
     }
 
     shufflecards(){
@@ -21,7 +26,14 @@ class Main extends Component{
 
     dealcards() {
         return this.state.cards.map((num, index) => {
-            return <Card key={index} num={num}/>;
+            return <Card key={index} num={num} display={this.state.display}/>;
+        });
+    }
+
+    reset(){
+        console.log('resetting');
+        this.setState({
+            reset: true
         });
     }
 
@@ -29,10 +41,9 @@ class Main extends Component{
         return(
             <div>Main Board
                 <Player />
-                <div>
-                    {this.dealcards()}
-                </div>
+                <Deck cards={this.state.cards} reset={this.state.reset}/>
                 <Boss />
+                <button onClick={this.reset}>Reset</button>
             </div>
         );
     }
