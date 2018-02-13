39745d7ca5dc0b31cc083f90da09bd27d14ac52b
michaeljlin <mjameslin@gmail.com>
2018-02-12 23:50:43 -0800

    Added animation to text shadow + changed to flexbox to better align start option

--- src/components/menu.js
+++ src/components/menu.js
@@ -46,6 +46,7 @@ class Menu extends Component{
                 <div className="scanlines"></div>
                 <div className="select">
                     <h1 style={clearStyle}>$ METAL GEAR SO$LID MEMORY MATCH</h1>
+                    <p style={clearStyle} className="startClick">Click Here to Start</p>
                 </div>
                 <div className="backgroundMenu" style={clearStyle}></div>
                 <div className="transitionMenu"></div>
