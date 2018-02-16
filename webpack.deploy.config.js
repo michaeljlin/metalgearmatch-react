c287107d5427630e1fc5b4dd0f5cc43c3477854a
michaeljlin <mjameslin@gmail.com>
2018-02-15 20:04:42 -0800

    Updated gitignore

--- webpack.deploy.config.js
+++ webpack.deploy.config.js
@@ -35,7 +35,7 @@ module.exports = {
                 }
             },
             {
-                test: /\.mp3$/,
+                test: /\.(mp3|wav)$/,
                 loader: 'file-loader'
             }
         ]
