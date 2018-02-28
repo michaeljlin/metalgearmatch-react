e7eb25ce2dae1da716a2ea860570aea0e8ab1220
michaeljlin <mjameslin@gmail.com>
2018-02-27 22:12:00 -0800

    Updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     ],
     output: {
         filename: 'bundle.js',
-        path: resolve(__dirname, 'dist'),
+        path: resolve(__dirname, 'docs'),
         publicPath: '/'
     },
     context: resolve(__dirname, 'src'),
